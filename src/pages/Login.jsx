f3e34a023ac4cfbf54c6ee41ac580dde6f6c4fa1
PremKhatri <premkhatri2672@gmail.com>
2026-06-03 16:06:33 +0530

    Remove Back to Home link from Login and Signup pages

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -41,13 +41,7 @@ const Login = () => {
       <div className="auth-orb auth-orb-2" />
       <div className="auth-orb auth-orb-3" />
 
-      {}
-      <Link to="/" className="auth-back-home">
-        <svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2">
-          <path d="M19 12H5M12 19l-7-7 7-7" strokeLinecap="round" strokeLinejoin="round" />
-        </svg>
-        Back to Home
-      </Link>
+
 
       <motion.div
         className="auth-card"
